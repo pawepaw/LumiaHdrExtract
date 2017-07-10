41faf43ef3364d26dbef2009c383eeacd80eeba1
Paweł Pawelec <pawelpawelec@o2.pl>
2017-07-10 20:58:06 +0200

    script to export from lumia hdr directories.

---
+++ LumiaHdrExtract.ps1
@@ -0,0 +1,11 @@
+﻿$sourceDir = dir d:\a | ?{$_.PSISContainer}
+$outputDir = "d:\output\"
+$sourceFileName = "\NaturalHDR.jpg"
+
+foreach ($nestedDir in $dir)
+{
+    $sourceFilePath = $nestedDir.FullName + $sourceFileName
+    $targetFileName = Split-Path -Leaf $nestedDir.FullName
+    $targetFilePath = $outputDir + $targetFileName +".jpg"
+    Copy-Item $sourceFilePath -Destination $targetFilePath 
+}
